860a71d35a7e569603af071d9c2453299ad00ccf
Charles-P. Clermont <charles@cpclermont.com>
2022-12-14 11:43:35 -0500

    oh shit!

--- packages/node/cli.ts
+++ packages/node/cli.ts
@@ -146,7 +146,7 @@ export async function check(root: string): Promise<Offense[]> {
 async function main(): Promise<void> {
   const root = process.argv[2];
   const offenses = await check(root);
-  console.log(JSON.stringify(offenses));
+  console.log(JSON.stringify(offenses, null, 2));
 }
 
 main();
