91dfa1eb6ddb13de3d24425065cc78332ee12b05
Charles-P. Clermont <charles@cpclermont.com>
2022-12-16 12:36:16 -0500

    Add browser scaffold

---
+++ packages/browser/src/index.ts
@@ -0,0 +1,84 @@
+import { toLiquidHtmlAST } from '@shopify/prettier-plugin-liquid/dist/parser/stage-2-ast';
+import toJSON from 'json-to-ast';
+
+import {
+  Config,
+  JSONSourceCode,
+  LiquidSourceCode,
+  Offense,
+  SourceCodeType,
+  Theme,
+  allChecks,
+  check as coreCheck,
+  recommended,
+} from '@shopify/theme-check-common';
+
+export { allChecks, recommended, Config };
+
+function toSourceCode(
+  relativePath: string,
+  source: string,
+): LiquidSourceCode | JSONSourceCode | undefined {
+  try {
+    const isLiquid = relativePath.endsWith('.liquid');
+
+    if (isLiquid) {
+      return {
+        absolutePath: '/' + relativePath,
+        relativePath,
+        source,
+        type: SourceCodeType.LiquidHtml,
+        ast: toLiquidHtmlAST(source),
+      };
+    } else {
+      return {
+        absolutePath: '/' + relativePath,
+        relativePath,
+        source,
+        type: SourceCodeType.JSON,
+        ast: toJSON(source),
+      };
+    }
+  } catch (e) {
+    return undefined;
+  }
+}
+
+/**
+ * @example
+ * {
+ *   'theme/layout.liquid': `
+ *     <html>
+ *       {{ content_for_page }}
+ *     </html>
+ *   `,
+ *   'snippets/snip.liquid': `
+ *     <b>'hello world'</b>
+ *   `,
+ * }
+ */
+export type ThemeData = {
+  [relativePath in string]: string;
+};
+
+export function getTheme(themeDesc: ThemeData): Theme {
+  const fileKVs: [string, LiquidSourceCode | JSONSourceCode | undefined][] = Object.entries(
+    themeDesc,
+  ).map(([relativePath, source]) => [relativePath, toSourceCode(relativePath, source)]);
+  return {
+    files: new Map(fileKVs.filter(([, v]) => !!v) as [string, LiquidSourceCode | JSONSourceCode][]),
+  };
+}
+
+/**
+ * In the event where you don't care about reusing your Theme object, simpleCheck works alright.
+ *
+ * But if you want to manage your memory (e.g. don't reparse ASTs for files that were not modified),
+ * it might be preferable to call coreCheck directly.
+ */
+export async function simpleCheck(themeDesc: ThemeData, config: Config): Promise<Offense[]> {
+  const theme = getTheme(themeDesc);
+  return coreCheck(theme, config);
+}
+
+export { coreCheck };
