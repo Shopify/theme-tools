f54afb151c482176d92b38f73215bef00a30c301
Charles-P. Clermont <charles@cpclermont.com>
2022-12-16 13:52:09 -0500

    Add playground

---
+++ playground/assets/playground.js
@@ -0,0 +1,31 @@
+function check() {
+  try {
+    ThemeCheck.simpleCheck(
+      {
+        'snippets/foo.liquid': input.value,
+      },
+      {
+        settings: {},
+        checks: ThemeCheck.recommended,
+      },
+    ).then((offenses) => {
+      output.value = JSON.stringify(offenses, null, 2);
+    });
+  } catch (error) {
+    // output.value = error.stack || error;
+  }
+}
+
+function onKeyup(e) {
+  if (e.ctrlKey && event.key === 'f') {
+    input.value = output.value;
+  }
+}
+
+async function main() {
+  check();
+  input.oninput = check;
+  input.addEventListener('keyup', onKeyup);
+}
+
+main();
