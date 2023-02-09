bef090a3307554fd93ee248197806edf4c06d4a7
Charles-P. Clermont <charles@cpclermont.com>
2023-02-09 16:44:56 -0500

    Sprinkle in vscode-languageserver

--- packages/browser/src/index.ts
+++ packages/browser/src/index.ts
@@ -1,17 +1,57 @@
 import { startServer as startCoreServer } from '@shopify/liquid-language-server-common';
+import {
+  createConnection,
+  Message,
+  MessageReader,
+  MessageWriter,
+  Disposable,
+} from 'vscode-languageserver/browser';
+
+const disposable = (dispose: () => void): Disposable => ({ dispose });
 
 // This is where you do the worker.postMessage stuff?
 // Or is this where we accept the worker.postMessage stuff?
 // Yeah I think this is where you _accept_ the worker.postMessage stuff
 export function startServer(channel: Worker) {
-  channel.addEventListener('message', ({ data: message }) => {
-    console.log('[worker]', message);
-    channel.postMessage({
-      id: 0,
-      result: {
-        capabilities: {},
-      },
-    });
-  });
-  startCoreServer();
+  // This is just ugly glue code that basically pipes the messages from the
+  // worker connection to the library. They have a very specific interface
+  // we need to map to, so that's what we're doing here.
+  const reader: MessageReader = {
+    onError(_listener) {
+      return disposable(() => {});
+    },
+    onClose(_listener) {
+      return disposable(() => {});
+    },
+    onPartialMessage(_listener) {
+      return disposable(() => {});
+    },
+    dispose() {},
+    listen(callback) {
+      const onMessage = (message: MessageEvent<any>) => callback(message.data);
+      channel.addEventListener('message', onMessage);
+      return disposable(() =>
+        channel.removeEventListener('message', onMessage),
+      );
+    },
+  };
+
+  // Glue code between our code and the vscode liquid language server
+  // It exists to map our messages to postMessage.
+  const writer: MessageWriter = {
+    onError(_listener) {
+      return disposable(() => {});
+    },
+    onClose(_listener) {
+      return disposable(() => {});
+    },
+    dispose() {},
+    end() {},
+    async write(msg: Message) {
+      channel.postMessage(msg);
+    },
+  };
+
+  const connection = createConnection(reader, writer);
+  startCoreServer(connection);
 }
--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,8 +1,21 @@
+import { Connection, InitializeResult } from 'vscode-languageserver';
+
 // Inject "boundary" stuff
 // But what is it?
 // - Filesystem
 // - Formatter
 // - The way it was built
-export function startServer() {
-  console.log('hello world');
+export function startServer(connection: Connection) {
+  connection.onInitialize((_params) => {
+    const result: InitializeResult = {
+      capabilities: {},
+      serverInfo: {
+        name: 'liquid-language-server',
+        version: '0.0.1',
+      },
+    };
+    return result;
+  });
+
+  connection.listen();
 }
