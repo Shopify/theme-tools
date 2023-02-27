8e5d11d50b750c1ebe9d3c30c76bbb8aa6c745ee
Charles-P. Clermont <charles@cpclermont.com>
2023-02-27 13:46:30 -0500

    Add mocha unit testing setup

---
+++ packages/browser/src/index.spec.ts
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+import { startServer } from './index'
+
+describe('Module: index (browser)', () => {
+  it('should exist and run', () => {
+    expect(startServer).to.exist;
+  });
+});
