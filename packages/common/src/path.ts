a91ec5d7802b7b228d2e1f56cc4df982048b8f83
Julien Poitrin <julien.poitrin@shopify.com>
2023-03-31 14:29:47 +0200

    Mention issue #15

--- packages/common/src/path.ts
+++ packages/common/src/path.ts
@@ -1,11 +1,11 @@
 import { RelativePath, AbsolutePath } from './types';
 
 export function relative(from: AbsolutePath, to: AbsolutePath): RelativePath {
-  // TODO do the right thing for windows and shit.
+  // TODO (#15): do the right thing for windows and shit.
   return from.replace(to, '');
 }
 
 export function join(...paths: string[]): string {
-  // TODO do the right thing, collapse slashes and shit.
+  // TODO (#15): do the right thing, collapse slashes and shit.
   return paths.join('/');
 }
