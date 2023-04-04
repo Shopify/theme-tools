d4682a68ab6aab48c56d86713c83f9abd7ae940b
Charles-P. Clermont <charles@cpclermont.com>
2023-04-04 09:39:27 -0400

    Re-export ThemeCheckConfig, allChecks, recommendedChecks in LLS

--- packages/browser/src/index.ts
+++ packages/browser/src/index.ts
@@ -12,6 +12,8 @@ import {
 
 const disposable = (dispose: () => void): Disposable => ({ dispose });
 
+export * from '@shopify/liquid-language-server-common';
+
 // This is where you do the worker.postMessage stuff?
 // Or is this where we accept the worker.postMessage stuff?
 // Yeah I think this is where you _accept_ the worker.postMessage stuff
--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,2 +1,9 @@
+import {
+  Config as ThemeCheckConfig,
+  allChecks,
+  recommended as recommendedChecks,
+} from '@shopify/theme-check-common';
+
 export * from './types';
 export { startServer } from './server';
+export { ThemeCheckConfig, recommendedChecks, allChecks };
