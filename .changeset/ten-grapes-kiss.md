a8cda19c206fcf25dd42cbd95ac127a409cd1066
Charles-P. Clermont <charles@cpclermont.com>
2023-04-19 12:49:40 -0400

    Add TranslationKeyExists to allChecks

---
+++ .changeset/ten-grapes-kiss.md
@@ -0,0 +1,7 @@
+---
+'@shopify/theme-check-browser': patch
+'@shopify/theme-check-common': patch
+'@shopify/theme-check-node': patch
+---
+
+Add TranslationKeyExists to allChecks array
--- packages/common/src/checks/index.ts
+++ packages/common/src/checks/index.ts
@@ -2,11 +2,13 @@ import { JSONCheckDefinition, LiquidCheckDefinition } from '@shopify/theme-check
 import { ParserBlockingScript } from './parser-blocking-script';
 import { MatchingTranslations } from './matching-translations';
 import { MissingTemplate } from './missing-template';
+import { TranslationKeyExists } from './translation-key-exists';
 
 export const allChecks: (LiquidCheckDefinition | JSONCheckDefinition)[] = [
   ParserBlockingScript,
   MatchingTranslations,
   MissingTemplate,
+  TranslationKeyExists,
 ];
 
 export const recommended = allChecks.filter((check) => check.meta.docs.recommended);
