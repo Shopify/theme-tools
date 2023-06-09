e4341d3023f4e0ddb18f384da13c882be279ff84
Charles-P. Clermont <charles@cpclermont.com>
2023-06-09 09:29:07 -0400

    Add benchmarking method

--- packages/node/src/cli.ts
+++ packages/node/src/cli.ts
@@ -4,8 +4,8 @@ import path from 'node:path';
 async function main(): Promise<void> {
   const root = path.resolve(process.argv[2]);
   const { config, offenses } = await themeCheckRun(root);
-  console.log(JSON.stringify(offenses, null, 2));
-  console.log(JSON.stringify(config.settings, null, 2));
+  // console.log(JSON.stringify(offenses, null, 2));
+  // console.log(JSON.stringify(config.settings, null, 2));
 }
 
 main();
