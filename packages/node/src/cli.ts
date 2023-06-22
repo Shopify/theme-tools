11c7b72c238fb3e615cf89be3231b3fcdba74eb0
Charles-P. Clermont <charles@cpclermont.com>
2023-06-22 14:41:17 -0400

    Include sanity check of user-provided check require

--- packages/node/src/cli.ts
+++ packages/node/src/cli.ts
@@ -3,7 +3,8 @@ import path from 'node:path';
 
 async function main(): Promise<void> {
   const root = path.resolve(process.argv[2]);
-  const { theme, config, offenses } = await themeCheckRun(root);
+  const configPath = process.argv[3] ? path.resolve(process.argv[3]) : undefined;
+  const { theme, config, offenses } = await themeCheckRun(root, configPath);
   console.log(JSON.stringify(offenses, null, 2));
   console.log(JSON.stringify(config, null, 2));
   console.log(
