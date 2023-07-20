8f2270f70aa7c35059cf8e1dca9a4af129d5c916
Albert Chu <albchu@users.noreply.github.com>
2023-07-20 15:59:10 -0600

    Small types fixes (#76)
    * Small types fixes
    
    * changeset
    
    * bumped verson of theme-check-common

---
+++ .changeset/brave-adults-march.md
@@ -0,0 +1,7 @@
+---
+'@shopify/liquid-language-server-common': minor
+'@shopify/liquid-language-server-node': minor
+'@shopify/liquid-language-server-browser': minor
+---
+
+Fixed: type of docset entry data rendered
--- packages/common/src/completions/providers/common/MarkdownRenderer.ts
+++ packages/common/src/completions/providers/common/MarkdownRenderer.ts
@@ -7,13 +7,11 @@ export function render(entry: DocsetEntry) {
 }
 
 function title(entry: DocsetEntry) {
-  return entry.link
-    ? `### [${entry.name}](${entry.link})`
-    : `### ${entry.name}`;
+  return `### ${entry.name}`;
 }
 
 function body(entry: DocsetEntry) {
-  return [entry.deprecationReason, entry.summary, entry.description]
+  return [entry.deprecation_reason, entry.summary, entry.description]
     .filter(exists)
     .join(HORIZONTAL_SEPARATOR);
 }
