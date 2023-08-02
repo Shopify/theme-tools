19e1b815ee561cac19ad2a27afccdaa569d173a5
Albert Chu <albert.chu@shopify.com>
2023-08-02 19:43:10 +0000

    Vitest setup for theme packages

---
+++ vitest.config.ts
@@ -0,0 +1,18 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import { configDefaults } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    exclude: [
+      ...configDefaults.exclude,
+      "packages/prettier-plugin-liquid/**", // TODO: Set up testing for this package.
+      "packages/vscode-extension/**", // TODO: Set up testing for this package.
+    ],
+    singleThread: true,
+    setupFiles: [
+      "./packages/theme-check-common/src/test/test-setup.ts",
+      "./packages/theme-language-server-common/src/test/test-setup.ts",
+    ],
+  },
+});
