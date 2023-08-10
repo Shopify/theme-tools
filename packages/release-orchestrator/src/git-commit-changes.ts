28999bc809ab698a60eff16f702022e795e4d41e
Albert Chu <albert.chu@shopify.com>
2023-08-10 14:44:46 -0600

    Release pipeline triggered by `yarn release`
    At a high level, the release pipeline stages are:
    - Sanity check to ensure it is executing from main branch and no untracked changes exist in the repo
    - checkout a release branch name in the structure of release/DD-MM-YY
    - Find all proposed package version updates and all the package jsons within the monorepo’s packages
    - Patch bump any repo packages that has a dependency with version updates
    - Commit those patch bumps (for easy inspection in subsequent PR)
    - Use changeset version to perform the actual version bumps
    - Push the release branch
    - Create a PR link to open the PR with prepopulated fields

---
+++ packages/release-orchestrator/src/git-commit-changes.ts
@@ -0,0 +1,43 @@
+import fs from 'fs';
+import path from 'path';
+import { readdir } from 'node:fs/promises';
+
+import { gitStatus, run } from './utils';
+
+import { confirmProceed } from './confirm-proceed';
+
+/**
+ * Returns a list of all the staged files in the repo.
+ */
+const stageChanges = async (): Promise<string[]> => {
+  const raw = await run('git diff --name-only --cached');
+  return raw.trim().split('\n').filter(Boolean);
+};
+
+export const gitCommitChanges =
+  (message: string, addPattern = ['*']) =>
+  async () => {
+    for (const pattern of addPattern) {
+      await run(`git add ${pattern}`);
+    }
+
+    const stagedChanges = await stageChanges();
+
+    if (!stagedChanges.length) {
+      return;
+    }
+
+    console.log('We need to make a git commit. Awaiting your approval...\n');
+    console.log(`${message}:\n\t-`, stagedChanges.join('\n\t- '));
+
+    const proceed = await confirmProceed();
+
+    if (proceed) {
+      console.log('Changes have been committed!');
+      return run(`git commit -m "${message}"`);
+    }
+
+    console.log('Changes have not been committed. Exiting...');
+    await run('git restore --staged .');
+    process.exit(0);
+  };
---
+++ packages/release-orchestrator/src/patch-bump-dependants.ts
@@ -0,0 +1,68 @@
+import type { ChangesetStatus, PackageJsonMap } from './types';
+import { generatePatchChangeset } from './generate-patch-changeset';
+import { changesetStatus } from './changeset';
+
+/**
+ * Creates patch changelogs for all packages that depend on the
+ * updated packages but were not updated themselves.
+ *
+ * This will enable us to leverage `changeset version` to bump everything.
+ */
+export const patchBumpDependants = async (packageJsonMap: PackageJsonMap) => {
+  let updated: ChangesetStatus;
+  let numPatches: number;
+
+  do {
+    /**
+     * `changeset status` will return the latest status of every package included
+     * in the next release based on all the changelogs available at the point of invocation.
+     */
+    updated = await changesetStatus();
+    numPatches = await applyPatchBumps({ updated, packageJsonMap });
+  } while (
+    /**
+     * Until there are no more patches that can be applied to un-updated packages
+     * we must keep evaluating whether more patches are needed.
+     */
+    numPatches > 0
+  );
+};
+
+const applyPatchBumps = async ({
+  updated,
+  packageJsonMap,
+}: {
+  updated: ChangesetStatus;
+  packageJsonMap: PackageJsonMap;
+}): Promise<number> => {
+  const updatedPkgNames = updated.releases.map((release) => release.name);
+
+  /**
+   * Perf opt: we can avoid evaluating packages that are already in the updatedPkgNames array
+   *
+   * Changeset configured with `"updateInternalDependencies": "patch"` will automatically bump the patch version
+   * of any monorepo package dependency that is already within the release.
+   */
+  const pkgNamesToProcess = Object.keys(packageJsonMap).filter(
+    (pkgName) => !updatedPkgNames.includes(pkgName),
+  );
+
+  let numPatches = 0;
+  const patchPromises = pkgNamesToProcess.map((pkgName) => {
+    const packageJson = packageJsonMap[pkgName];
+
+    // Check the package's dependencies for any updated package names
+    const updatedDeps = packageJson.dependencies
+      ? Object.keys(packageJson.dependencies).filter((dep) => updatedPkgNames.includes(dep))
+      : [];
+
+    if (updatedDeps.length) {
+      numPatches++;
+      return generatePatchChangeset(packageJson.name, updatedDeps);
+    }
+  });
+
+  await Promise.all(patchPromises);
+
+  return numPatches;
+};
