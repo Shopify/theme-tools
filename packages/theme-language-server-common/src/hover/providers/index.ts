d2edf2ff5ce5036d3d7db3729ad3c8da2052a52b
Charles-P. Clermont <charles@cpclermont.com>
2023-08-22 09:45:38 -0400

    Format

--- packages/theme-language-server-common/src/hover/providers/index.ts
+++ packages/theme-language-server-common/src/hover/providers/index.ts
@@ -4,4 +4,4 @@ export { LiquidObjectHoverProvider } from './LiquidObjectHoverProvider';
 export { LiquidObjectAttributeHoverProvider } from './LiquidObjectAttributeHoverProvider';
 export { HtmlTagHoverProvider } from './HtmlTagHoverProvider';
 export { HtmlAttributeHoverProvider } from './HtmlAttributeHoverProvider';
-export { HtmlAttributeValueHoverProvider} from './HtmlAttributeValueHoverProvider';
+export { HtmlAttributeValueHoverProvider } from './HtmlAttributeValueHoverProvider';
