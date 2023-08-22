ec64967b891a29c37e522e2749d2a58a506fd7eb
Charles-P. Clermont <charles@cpclermont.com>
2023-08-22 09:41:22 -0400

    Add LiquidObjectAttributeHoverProvider

---
+++ packages/theme-language-server-common/src/hover/providers/LiquidObjectAttributeHoverProvider.ts
@@ -0,0 +1,56 @@
+import { Hover, HoverParams } from 'vscode-languageserver';
+import { LiquidHtmlNode } from '@shopify/theme-check-common';
+import { NodeTypes } from '@shopify/prettier-plugin-liquid/dist/types';
+import { render } from '../../completions/providers/common';
+import { BaseHoverProvider } from '../BaseHoverProvider';
+import { TypeSystem, isArrayType } from '../../TypeSystem';
+
+export class LiquidObjectAttributeHoverProvider implements BaseHoverProvider {
+  constructor(private typeSystem: TypeSystem) {}
+
+  async hover(
+    _params: HoverParams,
+    currentNode: LiquidHtmlNode,
+    ancestors: LiquidHtmlNode[],
+  ): Promise<Hover | null> {
+    const parentNode = ancestors.at(-1);
+    if (
+      currentNode.type !== NodeTypes.String ||
+      !parentNode ||
+      parentNode.type !== NodeTypes.VariableLookup ||
+      !parentNode.lookups.includes(currentNode)
+    ) {
+      return null;
+    }
+
+    const lookupIndex = parentNode.lookups.findIndex((lookup) => lookup === currentNode);
+    const node = {
+      ...parentNode,
+      lookups: parentNode.lookups.slice(0, lookupIndex),
+    };
+
+    const parentType = await this.typeSystem.inferType(node, ancestors[0]);
+    if (isArrayType(parentType)) {
+      return null;
+    }
+
+    const objectMap = await this.typeSystem.objectMap();
+    const parentEntry = objectMap[parentType];
+    if (!parentEntry) {
+      return null;
+    }
+
+    const parentTypeProperties = objectMap[parentType]?.properties || [];
+    const entry = parentTypeProperties.find(p => p.name === currentNode.value)
+    if (!entry) {
+      return null;
+    }
+
+    return {
+      contents: {
+        kind: 'markdown',
+        value: render(entry),
+      },
+    };
+  }
+}
--- packages/theme-language-server-common/src/hover/providers/index.ts
+++ packages/theme-language-server-common/src/hover/providers/index.ts
@@ -1,3 +1,4 @@
 export { LiquidTagHoverProvider } from './LiquidTagHoverProvider';
 export { LiquidFilterHoverProvider } from './LiquidFilterHoverProvider';
 export { LiquidObjectHoverProvider } from './LiquidObjectHoverProvider';
+export { LiquidObjectAttributeHoverProvider } from './LiquidObjectAttributeHoverProvider';
