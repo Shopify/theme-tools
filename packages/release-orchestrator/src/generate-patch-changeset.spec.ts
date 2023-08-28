a39fd6cbcd6fe6d5f23e1e9e69e3359c8561442b
Albert Chu <albert.chu@shopify.com>
2023-08-28 12:11:41 -0600

    Calm down unit tests for windows

--- packages/release-orchestrator/src/generate-patch-changeset.spec.ts
+++ packages/release-orchestrator/src/generate-patch-changeset.spec.ts
@@ -6,9 +6,17 @@ import { generatePatchChangeset } from './generate-patch-changeset';
 import { getRepoRoot } from './utils';
 
 // Need to do this for stupid windows paths
-const expectedChangelogPath = path.join('/path', 'to', 'repo', '.changeset', '123456.md');
-const expectedChangesetPath = path.join('/path', 'to', 'repo', '.changeset');
-const expectedRepoPath = path.join('/path', 'to', 'repo');
+const repoPathTokens = ['path', 'to', 'repo'];
+const expectedRepoPath = path.join(...repoPathTokens); // path.join gives windows friendly pathing
+
+// Regex paths are for verifying paths in a cross-platform way
+const pathSeparatorToken = '[\\\\//]';
+const changesetPathRegex = new RegExp(
+  pathSeparatorToken + ['path', 'to', 'repo', '.changeset'].join(pathSeparatorToken),
+);
+const changelogPathRegex = new RegExp(
+  pathSeparatorToken + ['path', 'to', 'repo', '.changeset', '123456.md'].join(pathSeparatorToken),
+);
 
 vi.mock('./utils', async () => ({
   getRepoRoot: vi.fn(),
@@ -37,11 +45,11 @@ describe('generatePatchChangeset', () => {
 
     await generatePatchChangeset('package1', ['dependency1']);
 
-    expect(fs.mkdir).toHaveBeenCalledWith(expect.stringMatching(expectedChangesetPath), {
+    expect(fs.mkdir).toHaveBeenCalledWith(expect.stringMatching(changesetPathRegex), {
       recursive: true,
     });
     expect(fs.writeFile).toHaveBeenCalledWith(
-      expectedChangelogPath,
+      expect.stringMatching(changelogPathRegex),
       `---
 "package1": patch
 ---
@@ -57,11 +65,11 @@ Patch bump because it depends on dependency1
 
     await generatePatchChangeset('package1', ['dependency1', 'dependency2']);
 
-    expect(fs.mkdir).toHaveBeenCalledWith(expect.stringMatching(expectedChangesetPath), {
+    expect(fs.mkdir).toHaveBeenCalledWith(expect.stringMatching(changesetPathRegex), {
       recursive: true,
     });
     expect(fs.writeFile).toHaveBeenCalledWith(
-      expectedChangelogPath,
+      expect.stringMatching(changelogPathRegex),
       `---
 "package1": patch
 ---
