07f7fc2633d92b04e15e65bc1d616c377e131973
Albert Chu <albert.chu@shopify.com>
2023-09-05 16:49:45 -0600

    Added graceful recovery when nothing to release

--- packages/release-orchestrator/src/changeset.ts
+++ packages/release-orchestrator/src/changeset.ts
@@ -17,8 +17,17 @@ export const changesetVersion = async () => {
 export const changesetStatus = async (): Promise<ChangesetStatus> => {
   const basefile = `changeset-status.json`;
 
-  await run(`yarn changeset status --output=${basefile}`);
-
+  try {
+    await run(`yarn changeset status --output=${basefile}`);
+  } catch (err) {
+    console.log(
+      'Failed to get changeset status. This can happen if there are no changesets to process for release.',
+    );
+    console.log(
+      "Exiting release process. Please run `yarn changeset status` to see what's going on.",
+    );
+    process.exit();
+  }
   const statusFilepath = path.join(await getRepoRoot(), basefile);
   const statusOutput = JSON.parse(await readFile(statusFilepath, 'utf-8'));
 
