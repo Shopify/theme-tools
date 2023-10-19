5d8c425406fd2ba928497a88d2a298a627da6309
Charles-P. Clermont <charles@cpclermont.com>
2023-10-19 11:58:59 -0400

    Revert adding detail to CompletionItem in language server

--- packages/theme-language-server-common/src/completions/providers/common/CompletionItemProperties.ts
+++ packages/theme-language-server-common/src/completions/providers/common/CompletionItemProperties.ts
@@ -43,13 +43,11 @@ function documentationProperties(
   docsetEntryType?: DocsetEntryType,
   entryType?: PseudoType | ArrayType,
 ): {
-  detail: string | undefined;
   documentation: MarkupContent;
 } {
   const value = render(entry, entryType, docsetEntryType);
 
   return {
-    detail: entry.summary,
     documentation: {
       kind: 'markdown',
       value,
