bb79d83ddd42d82a6cc19ef296427da8d8bcb3ce
Tobias Mønster Madsen <67258103+tmmgrafikr@users.noreply.github.com>
2024-06-06 14:17:24 +0200

    Add `VariableName` check (#359)
    * Add VariableName check
    
    * Add changeset for my changes
    
    * Add docs URL
    
    * Add changeset for my changes
    
    * Remove legacy changeset
    
    * Update packages/theme-check-common/src/checks/variable-name/index.ts
    
    Co-authored-by: Guilherme Carreiro <karreiro@gmail.com>
    
    * Remove lodash package
    
    * Fix identical naming occurences
    
    * Formatting
    
    * Update packages/theme-check-common/src/checks/variable-name/index.ts
    
    Co-authored-by: Guilherme Carreiro <karreiro@gmail.com>
    
    ---------
    
    Co-authored-by: Guilherme Carreiro <karreiro@gmail.com>

---
+++ packages/theme-check-common/src/checks/variable-name/index.spec.ts
@@ -0,0 +1,45 @@
+import { expect, describe, it } from 'vitest';
+import { VariableName } from './index';
+import { applySuggestions, runLiquidCheck } from '../../test';
+
+describe('Module: VariableName', () => {
+  it('should report an error when a variable is using wrong naming format', async () => {
+    const sourceCode = `{% assign variableName = "value" %}`;
+
+    const offenses = await runLiquidCheck(VariableName, sourceCode);
+
+    expect(offenses).to.have.length(1);
+    expect(offenses[0].message).to.equal("The variable 'variableName' uses wrong naming format");
+  });
+
+  it('should not report an error when a variable is using correct format', async () => {
+    const sourceCode = `{% assign variable_name = "value" %}`;
+
+    const offenses = await runLiquidCheck(VariableName, sourceCode);
+
+    expect(offenses).to.be.empty;
+  });
+
+  it('should provide a suggestion to change the variable naming', async () => {
+    const sourceCode = `{% assign variableName = "value" %}`;
+
+    const offenses = await runLiquidCheck(VariableName, sourceCode);
+
+    expect(offenses).to.have.length(1);
+    expect(offenses[0].suggest).to.have.length(1);
+    expect(offenses[0]!.suggest![0].message).to.equal(
+      "Change variable 'variableName' to 'variable_name'",
+    );
+  });
+
+  it('should fix the code by changing the wrongly formatted variable', async () => {
+    const sourceCode = `{% assign variableName = "value" %}`;
+
+    const offenses = await runLiquidCheck(VariableName, sourceCode);
+    const suggestions = applySuggestions(sourceCode, offenses[0]);
+
+    const expectedFixedCode = `{% assign variable_name = "value" %}`;
+
+    expect(suggestions).to.include(expectedFixedCode);
+  });
+});
---
+++ packages/theme-check-common/src/checks/variable-name/index.ts
@@ -0,0 +1,114 @@
+import {
+  LiquidHtmlNode,
+  LiquidTag,
+  LiquidTagAssign,
+  LiquidTagCapture,
+  NodeTypes,
+} from '@shopify/liquid-html-parser';
+import { LiquidCheckDefinition, SchemaProp, Severity, SourceCodeType } from '../../types';
+
+import camelCase from 'lodash/camelCase';
+import kebabCase from 'lodash/kebabCase';
+import snakeCase from 'lodash/snakeCase';
+
+const pascalCase = (string: string) => {
+  const camelCased = camelCase(string);
+  return camelCased.charAt(0).toUpperCase() + camelCased.slice(1);
+};
+
+const isLiquidTagAssign = (node: LiquidTag): node is LiquidTagAssign => {
+  return node.name === 'assign' && typeof node.markup !== 'string';
+};
+
+const isLiquidTagCapture = (node: LiquidHtmlNode): node is LiquidTagCapture => {
+  return (
+    node.type == NodeTypes.LiquidTag && node.name === 'capture' && typeof node.markup !== 'string'
+  );
+};
+
+const formatTypes = {
+  camelCase: camelCase,
+  PascalCase: pascalCase,
+  snake_case: snakeCase,
+  'kebab-case': kebabCase,
+};
+
+type FormatTypes = keyof typeof formatTypes;
+
+const schema = {
+  format: SchemaProp.string('snake_case'),
+};
+
+export const VariableName: LiquidCheckDefinition<typeof schema> = {
+  meta: {
+    code: 'VariableName',
+    name: 'Invalid variable naming format',
+    docs: {
+      description: 'This check is aimed at using certain variable naming conventions',
+      url: 'https://shopify.dev/docs/themes/tools/theme-check/checks/variable-name',
+      recommended: true,
+    },
+    type: SourceCodeType.LiquidHtml,
+    severity: Severity.WARNING,
+    schema,
+    targets: [],
+  },
+
+  create(context) {
+    const formatter = (node: LiquidTagAssign | LiquidTagCapture) => {
+      if (!node.markup.name) {
+        return {
+          valid: false,
+        };
+      }
+
+      const suggestion = formatTypes[context.settings.format as FormatTypes].call(
+        null,
+        node.markup.name,
+      );
+
+      return {
+        valid: node.markup.name === suggestion,
+        suggestion,
+      };
+    };
+
+    const reportHandler = (node: LiquidTagAssign | LiquidTagCapture) => {
+      return context.report({
+        message: `The variable '${node.markup.name}' uses wrong naming format`,
+        startIndex: node.markup.position.start,
+        endIndex: node.markup.position.end,
+        suggest: [
+          {
+            message: `Change variable '${node.markup.name}' to '${formatter(node).suggestion}'`,
+            fix: (corrector) => {
+              const { position, name, source } = node.markup;
+
+              return corrector.replace(
+                position.start,
+                position.end,
+                source
+                  .slice(position.start, position.end)
+                  .replace(name!, formatter(node).suggestion!),
+              );
+            },
+          },
+        ],
+      });
+    };
+
+    return {
+      async LiquidTag(node) {
+        if (isLiquidTagAssign(node)) {
+          if (!formatter(node).valid) {
+            reportHandler(node);
+          }
+        } else if (isLiquidTagCapture(node) && node.markup.name) {
+          if (!formatter(node).valid) {
+            reportHandler(node);
+          }
+        }
+      },
+    };
+  },
+};
