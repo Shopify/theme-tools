3fc0361f9606744a76fd5b5fa8a19f935c16b451
Mateus Jabour <mjabourbp@gmail.com>
2024-06-12 14:11:40 -0400

    Address comments 2

--- .changeset/silly-sheep-happen.md
+++ .changeset/silly-sheep-happen.md
@@ -2,4 +2,4 @@
 '@shopify/theme-check-common': major
 ---
 
-Add `UniqueStaticBlockId` check which ensures the uniqueness of ids for static blocks defined in the same section/block file"
+Add `UniqueStaticBlockId` check which ensures the uniqueness of ids for static blocks defined in the same section/block file
--- packages/theme-check-common/src/checks/unique-static-block-id/index.spec.ts
+++ packages/theme-check-common/src/checks/unique-static-block-id/index.spec.ts
@@ -27,4 +27,29 @@ describe('Module: UniqueStaticBlockId', () => {
 
     expect(offenses).to.have.length(0);
   });
+
+  it('should report an error when two static blocks have same id and different types', async () => {
+    const sourceCode = `
+     {% content_for "block", type: "text", id: "static-block" %}
+     {% content_for "block", type: "url", id: "static-block" %}
+    `;
+
+    const offenses = await runLiquidCheck(UniqueStaticBlockId, sourceCode);
+
+    expect(offenses).to.have.length(1);
+    expect(offenses[0].message).to.equal(
+      "The id 'static-block' is already being used by another static block",
+    );
+  });
+
+  it('should not report an error when two static blocks have different id and different types', async () => {
+    const sourceCode = `
+     {% content_for "block", type: "text", id: "static-block1" %}
+     {% content_for "block", type: "url", id: "static-block2" %}
+    `;
+
+    const offenses = await runLiquidCheck(UniqueStaticBlockId, sourceCode);
+
+    expect(offenses).to.have.length(0);
+  });
 });
