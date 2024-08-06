098519ab7fb414834c5c747ad319015229130520
CP Clermont <cp.clermont@shopify.com>
2024-08-06 14:58:04 -0400

    Add `@shopify/lang-jsonc` (#320)

---
+++ packages/lang-jsonc/src/highlight.ts
@@ -0,0 +1,14 @@
+import { styleTags, tags as t } from '@lezer/highlight';
+
+export const jsonHighlighting = styleTags({
+  String: t.string,
+  Number: t.number,
+  'True False': t.bool,
+  PropertyName: t.propertyName,
+  Null: t.null,
+  ',': t.separator,
+  '[ ]': t.squareBracket,
+  '{ }': t.brace,
+  LineComment: t.lineComment,
+  BlockComment: t.blockComment,
+});
