dd173cdde0643c0aede47fc9e2614611acdc57c5
Charles-P. Clermont <charles@cpclermont.com>
2024-10-02 15:05:39 -0400

    Add Liquid Tag Snippet Completion
    Fixes #500

--- packages/theme-language-server-common/src/completions/providers/LiquidTagsCompletionProvider.spec.ts
+++ packages/theme-language-server-common/src/completions/providers/LiquidTagsCompletionProvider.spec.ts
@@ -1,12 +1,80 @@
 import { describe, beforeEach, it, expect } from 'vitest';
 import { DocumentManager } from '../../documents';
 import { CompletionsProvider } from '../CompletionsProvider';
+import { TagEntry } from '@shopify/theme-check-common';
+import { InsertTextFormat, InsertTextMode, TextEdit } from 'vscode-languageserver-protocol';
+import { TextDocument } from 'vscode-languageserver-textdocument';
+import { CURSOR } from '../params';
 
-export const tags = [
-  { name: 'render' },
-  { name: 'for' },
-  { name: 'comment' },
-  { name: 'if' },
+const caseSyntax = `{% case variable %}
+  {% when first_value %}
+    first_expression
+  {% else %}
+    second_expression
+{% endcase %}`;
+const caseSnippetWithInsideStripping = `case \${1:variable} -%}
+  {%- when \${2:first_value} -%}
+    \$3
+  {%- else -%}
+    \$4
+{%- endcase %}`;
+const caseSnippetWithOutsideStripping = `case \${1:variable} %}
+  {% when \${2:first_value} %}
+    \$3
+  {% else %}
+    \$4
+{% endcase -%}`;
+const caseSnippetWithAllTheStripping = `case \${1:variable} -%}
+  {%- when \${2:first_value} -%}
+    \$3
+  {%- else -%}
+    \$4
+{%- endcase -%}`;
+const caseSnippetInsideLiquidTag = `case \${1:variable}
+  when \${2:first_value}
+    \$3
+  else
+    \$4
+endcase`;
+
+export const tags: TagEntry[] = [
+  {
+    name: 'render',
+    syntax: "{% render 'snippet' %}",
+    syntax_keywords: [{ keyword: 'snippet', description: '...' }],
+  },
+  {
+    name: 'for',
+    syntax: '{% for item in array) %}body{% endfor %}',
+    syntax_keywords: [
+      { keyword: 'item', description: '...' },
+      { keyword: 'array', description: '...' },
+      { keyword: 'body', description: '...' },
+    ],
+  },
+  {
+    name: 'case',
+    syntax: caseSyntax,
+    syntax_keywords: [
+      { keyword: 'variable', description: '...' },
+      { keyword: 'first_value', description: '...' },
+      { keyword: 'first_expression', description: '...' },
+      { keyword: 'second_expression', description: '...' },
+    ],
+  },
+  {
+    name: 'comment',
+    syntax: '{% comment %}comment_body{% endcomment %}',
+    syntax_keywords: [{ keyword: 'comment_body', description: '...' }],
+  },
+  {
+    name: 'if',
+    syntax: '{% if condition %}\n  expression\n{% endif %}',
+    syntax_keywords: [
+      { keyword: 'condition', description: '...' },
+      { keyword: 'expression', description: '...' },
+    ],
+  },
   { name: 'echo' },
 ];
 
@@ -102,6 +170,200 @@ describe('Module: LiquidTagsCompletionProvider', async () => {
       '{% form "cart", cart %} ... {% ',
       allTags.concat('endform'),
     );
+  });
+
+  describe('Snippet completion', () => {
+    it('should not snippet complete when the tag being completed already has markup in it', async () => {
+      const scenarios = [
+        '{% if█ a > b %}',
+        '{% if█ a > b -%}',
+        `{% liquid
+           if█ a > b
+         %}`,
+      ];
+      for (const source of scenarios) {
+        await expect(provider).to.complete(source, [
+          expect.objectContaining({
+            label: 'if',
+            insertTextFormat: InsertTextFormat.PlainText,
+          }),
+        ]);
+      }
+    });
+
+    it('should snippet complete when the tag being completed doesnt have existing markup in it', async () => {
+      const scenarios = [
+        '{% if█ %}',
+        '{% if█ -%}...{% if cond %}{% endif %}',
+        '{% if cond %}{% if█ -%}...{% endif %}',
+      ];
+      for (const scenario of scenarios) {
+        await expect(provider).to.complete(scenario, [
+          expect.objectContaining({
+            label: 'if',
+            insertTextFormat: InsertTextFormat.Snippet,
+          }),
+        ]);
+      }
+    });
+
+    it('should offer text edits that apply correctly', async () => {
+      const source = '...\n{% if█ %}\n...';
+      const textEdit: TextEdit = {
+        newText: 'if ${1:condition} %}\n  $2\n{% endif %}',
+        range: {
+          start: { line: 1, character: 3 },
+          end: { line: 1, character: 8 },
+        },
+      };
+      await expect(provider).to.complete(source, [
+        expect.objectContaining({
+          label: 'if',
+          insertTextFormat: InsertTextFormat.Snippet,
+          insertTextMode: InsertTextMode.adjustIndentation,
+          textEdit,
+        }),
+      ]);
+      const textDocument = TextDocument.create('', 'liquid', 0, source.replace(CURSOR, ''));
+      expect(TextDocument.applyEdits(textDocument, [textEdit])).toBe(
+        '...\n{% if ${1:condition} %}\n  $2\n{% endif %}\n...',
+      );
+    });
+
+    it('should inline the snippet if the tag is inline with more content', async () => {
+      await expect(provider).to.complete('<div {% if█ %}>', [
+        expect.objectContaining({
+          label: 'if',
+          insertTextFormat: InsertTextFormat.Snippet,
+          textEdit: {
+            range: expect.any(Object),
+            newText: 'if ${1:condition} %}$2{% endif %}',
+          },
+        }),
+      ]);
+    });
+
+    it('should maintain inside stripping whitespace if present', async () => {
+      await expect(provider).to.complete('{% if█ -%}', [
+        expect.objectContaining({
+          label: 'if',
+          insertTextFormat: InsertTextFormat.Snippet,
+          textEdit: {
+            range: expect.any(Object),
+            newText: 'if ${1:condition} -%}\n  $2\n{%- endif %}',
+          },
+        }),
+      ]);
+      await expect(provider).to.complete('{% case█ -%}...', [
+        expect.objectContaining({
+          label: 'case',
+          insertTextFormat: InsertTextFormat.Snippet,
+          textEdit: {
+            range: expect.any(Object),
+            newText: caseSnippetWithInsideStripping,
+          },
+        }),
+      ]);
+    });
+
+    it('should mirror outsite stripping whitespace if present', async () => {
+      await expect(provider).to.complete('{%- if█ %}', [
+        expect.objectContaining({
+          label: 'if',
+          insertTextFormat: InsertTextFormat.Snippet,
+          textEdit: {
+            range: expect.any(Object),
+            newText: 'if ${1:condition} %}\n  $2\n{% endif -%}',
+          },
+        }),
+      ]);
+      await expect(provider).to.complete('{%- case█ %}...', [
+        expect.objectContaining({
+          label: 'case',
+          insertTextFormat: InsertTextFormat.Snippet,
+          textEdit: {
+            range: expect.any(Object),
+            newText: caseSnippetWithOutsideStripping,
+          },
+        }),
+      ]);
+    });
+
+    it('should default to the whitespace stripping of the outer bracket', async () => {
+      await expect(provider).to.complete('{%- if█', [
+        expect.objectContaining({
+          label: 'if',
+          insertTextFormat: InsertTextFormat.Snippet,
+          textEdit: {
+            range: expect.any(Object),
+            newText: 'if ${1:condition} -%}\n  $2\n{%- endif -%}',
+          },
+        }),
+      ]);
+    });
+
+    it('should strip whitespace everywhere if we can infer that', async () => {
+      await expect(provider).to.complete('{%- case█ -%}', [
+        expect.objectContaining({
+          label: 'case',
+          insertTextFormat: InsertTextFormat.Snippet,
+          textEdit: {
+            range: expect.any(Object),
+            newText: caseSnippetWithAllTheStripping,
+          },
+        }),
+      ]);
+    });
+
+    it('should snippet complete non-block tags correctly', async () => {
+      await expect(provider).to.complete('{% re█ -%}', [
+        expect.objectContaining({
+          label: 'render',
+          insertTextFormat: InsertTextFormat.Snippet,
+          textEdit: {
+            range: expect.any(Object),
+            newText: "render '$1'$2 -%}",
+          },
+        }),
+      ]);
+    });
+
+    describe('inside {% liquid %} tags', () => {
+      it('should remove the `{%` and `%}` from the snippet', async () => {
+        const source = `
+          {% liquid
+            if a > b
+              █
+            endif
+          %}`;
+        await expect(provider).to.complete(
+          source,
+          expect.arrayContaining([
+            expect.objectContaining({
+              label: 'if',
+              textEdit: {
+                range: expect.any(Object),
+                newText: 'if ${1:condition}\n  $2\nendif',
+              },
+            }),
+            expect.objectContaining({
+              label: 'render',
+              textEdit: {
+                range: expect.any(Object),
+                newText: "render '$1'$2",
+              },
+            }),
+            expect.objectContaining({
+              label: 'case',
+              textEdit: {
+                range: expect.any(Object),
+                newText: caseSnippetInsideLiquidTag,
+              },
+            }),
+          ]),
+        );
+      });
+    });
   });
 
   it('should not complete when the cursor position is not on the name', async () => {
