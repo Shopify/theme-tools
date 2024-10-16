18c95dded41e79de85a87ac906293a8260110326
Charles-P. Clermont <charles@cpclermont.com>
2024-10-16 15:39:23 -0400

    Implement and setup basic VsCodeFileSystem
    - Adds support for VS Code for Web
    - Adds support for remote files

--- packages/theme-language-server-browser/src/index.ts
+++ packages/theme-language-server-browser/src/index.ts
@@ -10,12 +10,19 @@ import {
 
 export * from '@shopify/theme-language-server-common';
 
+export function getConnection(worker: Worker) {
+  const reader = new BrowserMessageReader(worker);
+  const writer = new BrowserMessageWriter(worker);
+  return createConnection(reader, writer);
+}
 // This is where you do the worker.postMessage stuff?
 // Or is this where we accept the worker.postMessage stuff?
 // Yeah I think this is where you _accept_ the worker.postMessage stuff
-export function startServer(worker: Worker, dependencies: Dependencies) {
-  const reader = new BrowserMessageReader(worker);
-  const writer = new BrowserMessageWriter(worker);
-  const connection = createConnection(reader, writer);
+export function startServer(
+  worker: Worker,
+  dependencies: Dependencies,
+  connection = getConnection(worker),
+) {
+  console.info('staging server', worker, dependencies, connection);
   startCoreServer(connection, dependencies);
 }
--- packages/vscode-extension/src/node/server.ts
+++ packages/vscode-extension/src/node/server.ts
@@ -1,6 +1,15 @@
-import { startServer } from '@shopify/theme-language-server-node';
+import { startServer, getConnection, NodeFileSystem } from '@shopify/theme-language-server-node';
+import { VsCodeFileSystem } from '../common/VsCodeFileSystem';
+import type { AbstractFileSystem } from '@shopify/theme-check-common';
 
-startServer();
+const connection = getConnection();
+
+// When the URI starts with `file://`, we can use the NodeFileSystem directly (it's faster)
+const fileSystems: Record<string, AbstractFileSystem> = {
+  file: NodeFileSystem,
+};
+
+startServer(connection, new VsCodeFileSystem(connection, fileSystems));
 
 process.on('uncaughtException', (e) => {
   console.error(e);
