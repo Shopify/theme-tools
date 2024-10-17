95da0b8ea2d42b5f3979894301685785ca6d007a
Charles-P. Clermont <charles@cpclermont.com>
2024-10-17 14:26:51 -0400

    Docs changes and webpack config cleanup

--- packages/vscode-extension/src/common/VsCodeFileSystem.ts
+++ packages/vscode-extension/src/common/VsCodeFileSystem.ts
@@ -2,10 +2,25 @@ import { AbstractFileSystem, FileTuple, FileStat } from '@shopify/theme-check-co
 import { Connection } from 'vscode-languageserver';
 import { URI } from 'vscode-uri';
 
+/** e.g. `file`, `vscode-vfs`, `github`, `browser` */
+export type UriScheme = string;
+
+/**
+ * An optimization we can use so the Language Server can avoid
+ * querying VS Code for information.
+ *
+ * In the desktop version of the extension, the `file` scheme gets mapped to the
+ * NodeFileSystem class.
+ *
+ * In the browser, this is not available, so the Language Server queries
+ * VS Code for the information.
+ */
+export type FileSystems = Record<UriScheme, AbstractFileSystem>;
+
 export class VsCodeFileSystem implements AbstractFileSystem {
   constructor(
     private connection: Connection,
-    private fileSystems: Record<string, AbstractFileSystem>,
+    private fileSystems: Record<UriScheme, AbstractFileSystem>,
   ) {}
 
   readDirectory(uriString: string): Promise<FileTuple[]> {
