27028cf5fd87738a2d51b25d4282327141033ed4
Mia Zbikowski <mia.zbikowski@shopify.com>
2024-11-06 16:23:31 -0500

    Name translations validation

--- packages/theme-check-common/src/checks/valid-schema-name/index.ts
+++ packages/theme-check-common/src/checks/valid-schema-name/index.ts
@@ -4,6 +4,23 @@ import { isError } from '../../utils';
 
 const MAX_SCHEMA_NAME_LENGTH = 25;
 
+//Duplicated from TranslationKeyExists, should extract to a shared util
+function keyExists(key: string, pointer: any) {
+    for (const token of key.split('.')) {
+      if (typeof pointer !== 'object') {
+        return false;
+      }
+  
+      if (!pointer.hasOwnProperty(token)) {
+        return false;
+      }
+  
+      pointer = pointer[token];
+    }
+  
+    return true;
+  }
+
 export const ValidSchemaName: LiquidCheckDefinition = {
   meta: {
     code: 'ValidSchemaName',
@@ -34,6 +51,35 @@ export const ValidSchemaName: LiquidCheckDefinition = {
             message: 'Schema name is too long (max 25 characters)',
             startIndex: node.blockStartPosition.end, //awaiting schema AST to have attribute positions
             endIndex: node.blockEndPosition.start,
+          });
+        }
+
+        if(schema.name.startsWith('t:')) {
+          let schemaLocales: any;
+          const translationKey = schema.name;
+          const defaultTranslations = await context.getDefaultTranslations();
+          const defaultLocale = await context.getDefaultLocale();
+          const systemTranslations = await context.themeDocset?.systemTranslations();
+          const systemTranslationsKeys = Object.keys(systemTranslations ?? {});
+          schemaLocales = schema.locales?.[defaultLocale];
+
+          if (
+            keyExists(translationKey, defaultTranslations) ||
+            keyExists(translationKey, schemaLocales) ||
+            systemTranslationsKeys.includes(translationKey)
+          ) {
+            return;
+          }
+
+          let message = `'${translationKey}' does not have a matching entry in 'locales/${defaultLocale}.default.json'`;
+          if (schemaLocales) {
+            message += ` or '${context.toRelativePath(context.file.uri)}'`;
+          }
+
+          context.report({
+            message,
+            startIndex: node.blockStartPosition.end, //awaiting schema AST to have attribute positions
+            endIndex: node.blockEndPosition.start
           });
         }
       },
