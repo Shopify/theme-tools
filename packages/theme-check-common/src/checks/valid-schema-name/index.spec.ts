73178308d2d0f7c68b2f9733ae74ac2e8b15035f
Mia Zbikowski <mia.zbikowski@shopify.com>
2024-11-11 17:28:57 -0500

    Verify schema name translation

--- packages/theme-check-common/src/checks/valid-schema-name/index.spec.ts
+++ packages/theme-check-common/src/checks/valid-schema-name/index.spec.ts
@@ -1,5 +1,5 @@
 import { expect, describe, it } from 'vitest';
-import { highlightedOffenses, runLiquidCheck } from '../../test';
+import { highlightedOffenses, runLiquidCheck, check } from '../../test';
 import { ValidSchemaName } from './index';
 
 const DEFAULT_FILE_NAME = 'sections/file.liquid';
@@ -16,10 +16,70 @@ describe('Module: ValidSchemaName', () => {
 
     const offenses = await runLiquidCheck(ValidSchemaName, sourceCode, DEFAULT_FILE_NAME);
     expect(offenses).toHaveLength(1);
-    expect(offenses[0].message).toEqual('Schema name is too long (max 25 characters)');
+    expect(offenses[0].message).toEqual(
+      "Schema name 'test-schema-name-that-is-too-long' is too long (max 25 characters)",
+    );
 
     const highlights = highlightedOffenses({ [DEFAULT_FILE_NAME]: sourceCode }, offenses);
     expect(highlights).to.have.length(1);
     expect(highlights[0]).toBe('"test-schema-name-that-is-too-long"');
+  });
+
+  it('reports no offenses with schema name translation that exists and is not over 25 chars long', async () => {
+    const offenses = await check(
+      {
+        'locales/en.default.schema.json': '{ "my_translation_key": "My translation is good."}',
+        'code.liquid': `
+          {% schema %}
+            {
+              "name": "t:my_translation_key"
+        } 
+      {% endschema %}`,
+      },
+      [ValidSchemaName],
+    );
+
+    expect(offenses).toHaveLength(0);
+  });
+
+  it('reports an offense with schema name translation is missing', async () => {
+    const offenses = await check(
+      {
+        'locales/en.default.schema.json': '{ "another_translation_key": "Another translation"}',
+        'code.liquid': `
+          {% schema %}
+            {
+              "name": "t:my_translation_key"
+            } 
+          {% endschema %}`,
+      },
+      [ValidSchemaName],
+    );
+
+    expect(offenses).toHaveLength(1);
+    expect(offenses[0].message).toEqual(
+      "'t:my_translation_key' does not have a matching entry in 'locales/en.default.schema.json'",
+    );
+  });
+
+  it('reports an offense with schema name translation that exists and is over 25 chars long', async () => {
+    const offenses = await check(
+      {
+        'locales/en.default.schema.json':
+          '{ "my_translation_key": "My translation is tooooooooo long."}',
+        'code.liquid': `
+          {% schema %}
+            {
+              "name": "t:my_translation_key"
+            } 
+          {% endschema %}`,
+      },
+      [ValidSchemaName],
+    );
+
+    expect(offenses).toHaveLength(1);
+    expect(offenses[0].message).toEqual(
+      "Schema name 'My translation is tooooooooo long.' from 'locales/en.default.schema.json' is too long (max 25 characters)",
+    );
   });
 });
--- packages/theme-check-common/src/checks/valid-schema-name/index.ts
+++ packages/theme-check-common/src/checks/valid-schema-name/index.ts
@@ -1,8 +1,8 @@
-import { JSONNode, LiquidCheckDefinition, Severity, SourceCodeType } from '../../types';
+import { LiteralNode, ValueNode } from 'json-to-ast';
+import { parseJSON } from '../../json';
 import { toJSONAST } from '../../to-source-code';
+import { JSONNode, LiquidCheckDefinition, Severity, SourceCodeType } from '../../types';
 import { visit } from '../../visitor';
-import { LiteralNode } from 'json-to-ast';
-import { ValueNode } from 'json-to-ast';
 
 const MAX_SCHEMA_NAME_LENGTH = 25;
 const ROOT_NODE_ANCESTORS_COUNT = 1;
@@ -14,7 +14,7 @@ export const ValidSchemaName: LiquidCheckDefinition = {
     docs: {
       description: 'This check is aimed at ensuring a valid schema name.',
       recommended: true,
-      url: 'https://shopify.dev/docs/storefronts/thesadmes/tools/theme-check/checks/valid-schema-name',
+      url: 'https://shopify.dev/docs/storefronts/themes/tools/theme-check/checks/valid-schema-name',
     },
     type: SourceCodeType.LiquidHtml,
     severity: Severity.ERROR,
@@ -35,28 +35,53 @@ export const ValidSchemaName: LiquidCheckDefinition = {
         );
 
         const jsonSchemaAst = toJSONAST(jsonSchemaString);
-        visit<SourceCodeType.JSON, void>(jsonSchemaAst as JSONNode, {
-          Property(nameNode, ancestors) {
+
+        const promises = visit<SourceCodeType.JSON, Promise<void>>(jsonSchemaAst as JSONNode, {
+          async Property(nameNode, ancestors) {
             if (
               nameNode.key.value === 'name' &&
               ancestors.length === ROOT_NODE_ANCESTORS_COUNT &&
               isLiteralNode(nameNode.value)
             ) {
               const name = getLiteralValue(nameNode.value);
+              const startIndex = node.blockStartPosition.end + getLiteralLocStart(nameNode.value);
+              const endIndex = node.blockStartPosition.end + getLiteralLocEnd(nameNode.value);
 
-              // We will be handling the translation length in a follow up
               if (!name.startsWith('t:') && name.length > MAX_SCHEMA_NAME_LENGTH) {
-                const startIndex = node.blockStartPosition.end + getLiteralLocStart(nameNode.value);
-                const endIndex = node.blockStartPosition.end + getLiteralLocEnd(nameNode.value);
                 context.report({
-                  message: 'Schema name is too long (max 25 characters)',
+                  message: `Schema name '${name}' is too long (max 25 characters)`,
                   startIndex: startIndex,
                   endIndex: endIndex,
                 });
               }
+
+              if (name.startsWith('t:')) {
+                const defaultLocale = await context.getDefaultLocale();
+                const key = name.replace('t:', '');
+                const defaultTranslations = await context.getDefaultSchemaTranslations();
+                const translation = getTranslation(key, defaultTranslations);
+
+                if (translation === undefined) {
+                  context.report({
+                    message: `'${name}' does not have a matching entry in 'locales/${defaultLocale}.default.schema.json'`,
+                    startIndex: startIndex,
+                    endIndex: endIndex,
+                  });
+                }
+
+                if (translation !== undefined && translation.length > MAX_SCHEMA_NAME_LENGTH) {
+                  context.report({
+                    message: `Schema name '${translation}' from 'locales/${defaultLocale}.default.schema.json' is too long (max 25 characters)`,
+                    startIndex: startIndex,
+                    endIndex: endIndex,
+                  });
+                }
+              }
             }
           },
         });
+
+        await Promise.all(promises);
       },
     };
   },
@@ -79,4 +104,20 @@ function getLiteralLocStart(node: LiteralNode): number {
 
 function getLiteralLocEnd(node: LiteralNode): number {
   return node.loc?.end.offset ?? 0;
+}
+
+function getTranslation(key: string, pointer: any) {
+  for (const token of key.split('.')) {
+    if (typeof pointer !== 'object') {
+      return undefined;
+    }
+
+    if (!pointer.hasOwnProperty(token)) {
+      return undefined;
+    }
+
+    pointer = pointer[token];
+  }
+
+  return pointer; //the last pointer is the translation
 }
