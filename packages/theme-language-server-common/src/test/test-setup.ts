6b21f2cd7b03d89e31b68391820c03716e4fcb51
Charles-P. Clermont <charles@cpclermont.com>
2024-12-03 11:03:30 -0500

    Add the `applyTextEdit` custom matcher to make unit testing easier

--- packages/theme-language-server-common/src/test/test-setup.ts
+++ packages/theme-language-server-common/src/test/test-setup.ts
@@ -1,5 +1,6 @@
 import { expect } from 'vitest';
+import { applyTextEdit } from './ApplyTextEditAssertion';
 import { complete } from './CompletionItemsAssertion';
 import { hover } from './HoverAssertion';
 
-expect.extend({ hover, complete });
+expect.extend({ hover, complete, applyTextEdit });
