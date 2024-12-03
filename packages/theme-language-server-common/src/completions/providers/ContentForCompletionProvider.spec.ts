55d7dd8afb9dd057ea39aae377ddb07780fd9c1f
Charles-P. Clermont <charles@cpclermont.com>
2024-12-03 11:03:57 -0500

    Add snippet completion of `content_for 'block'`
    Will append the `type: '$1', id: '$2'`

--- packages/theme-language-server-common/src/completions/providers/ContentForCompletionProvider.spec.ts
+++ packages/theme-language-server-common/src/completions/providers/ContentForCompletionProvider.spec.ts
@@ -1,7 +1,8 @@
-import { describe, beforeEach, it, expect } from 'vitest';
-import { CompletionsProvider } from '../CompletionsProvider';
-import { DocumentManager } from '../../documents';
 import { MetafieldDefinitionMap } from '@shopify/theme-check-common';
+import { beforeEach, describe, expect, it } from 'vitest';
+import { InsertTextFormat } from 'vscode-json-languageservice';
+import { DocumentManager } from '../../documents';
+import { CompletionsProvider } from '../CompletionsProvider';
 
 describe('Module: ContentForCompletionProvider', async () => {
   let provider: CompletionsProvider;
@@ -22,5 +23,75 @@ describe('Module: ContentForCompletionProvider', async () => {
   it('should complete content_for block keywords correctly', async () => {
     const expected = ['block', 'blocks'].sort();
     await expect(provider).to.complete('{% content_for "█" %}', expected);
+  });
+
+  describe('when the text being completed does not have extra markup', () => {
+    it('should text edit complete content_for block with type & id placeholders', async () => {
+      const source = '{% content_for "█" %}';
+      await expect(provider).to.complete(
+        source,
+        expect.arrayContaining([
+          expect.objectContaining({
+            label: 'block',
+            textEdit: expect.applyTextEdit(
+              source,
+              `{% content_for "block", type: "$1", id: "$2" %}`,
+            ),
+          }),
+        ]),
+      );
+    });
+
+    it('should keep using the quote type used', async () => {
+      const source = `{% content_for '█' %}`;
+      await expect(provider).to.complete(
+        source,
+        expect.arrayContaining([
+          expect.objectContaining({
+            label: 'block',
+            textEdit: expect.applyTextEdit(
+              source,
+              // single quotes everywhere
+              `{% content_for 'block', type: '$1', id: '$2' %}`,
+            ),
+          }),
+        ]),
+      );
+    });
+
+    it('should text edit complete inside the liquid liquid tag', async () => {
+      const source = `{% liquid
+        content_for "█"
+        echo "more content"
+      %}`;
+      const afterEdit = `{% liquid
+        content_for "block", type: "$1", id: "$2"
+        echo "more content"
+      %}`;
+      await expect(provider).to.complete(
+        source,
+        expect.arrayContaining([
+          expect.objectContaining({
+            label: 'block',
+            textEdit: expect.applyTextEdit(source, afterEdit),
+          }),
+        ]),
+      );
+    });
+  });
+
+  describe('when it has extra markup', () => {
+    it('should not text edit complete', async () => {
+      const source = '{% content_for "█", type: "some-type", id: "some-id" %}';
+      await expect(provider).to.complete(
+        source,
+        expect.arrayContaining([
+          expect.objectContaining({
+            label: 'block',
+            insertTextFormat: InsertTextFormat.PlainText,
+          }),
+        ]),
+      );
+    });
   });
 });
