be7ef26d92759f04e5351ed260070342c45802f9
Guilherme Carreiro <guilherme.carreiro@shopify.com>
2025-01-24 12:57:37 +0100

    Setup 'vitest' on 'packages/vscode-extension'

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -5,9 +5,11 @@ const CI = !!process.env.CI;
 /** In CI prettier plugin tests are covered by a different run command */
 const ciExclude = ['./packages/prettier-plugin-liquid'];
 
+const exclude = [...configDefaults.exclude, './packages/vscode-extension/out/**'];
+
 export default defineConfig({
   test: {
-    exclude: CI ? [...configDefaults.exclude, ...ciExclude] : configDefaults.exclude,
+    exclude: CI ? [...exclude, ...ciExclude] : exclude,
     pool: 'forks',
     poolOptions: {
       forks: {
