c310c2eb728bc813670b15f5ec2b0d3f50c23477
Guilherme Carreiro <guilherme.carreiro@shopify.com>
2025-01-24 13:28:52 +0100

    Extract the logic for showing/hiding the 'Shopify Magic' button to 'ui.ts'

---
+++ packages/vscode-extension/src/node/ui.spec.ts
@@ -0,0 +1,67 @@
+import { it, expect, describe, vi, afterEach, afterAll } from 'vitest';
+import { showShopifyMagicButton, showShopifyMagicLoadingButton } from './ui';
+import { commands } from 'vscode';
+import { hasShopifyThemeLoaded } from './utils';
+
+vi.mock('./utils');
+vi.mock('vscode', async () => {
+  const commands = {
+    executeCommand: vi.fn(),
+  };
+  const workspace = {
+    workspaceFolders: [{ uri: { fsPath: '/mock/path' } }],
+    fs: {
+      stat: vi.fn(),
+    },
+  };
+  return { commands, workspace };
+});
+
+describe('ui', async () => {
+  afterEach(vi.clearAllMocks);
+  afterAll(vi.unstubAllGlobals);
+
+  describe('showShopifyMagicButton', async () => {
+    it('should show the magic button and hide the loading component when in a theme directory', async () => {
+      vi.mocked(hasShopifyThemeLoaded).mockResolvedValue(true);
+
+      await showShopifyMagicButton();
+
+      const shopifyMagicButtton = 'shopifyLiquid.shopifyMagic.visible';
+      const shopifyMagicLoading = 'shopifyLiquid.shopifyMagicLoading.visible';
+
+      expect(commands.executeCommand).toBeCalledWith('setContext', shopifyMagicButtton, true);
+      expect(commands.executeCommand).toBeCalledWith('setContext', shopifyMagicLoading, false);
+    });
+
+    it('should not update button visibility when not in a theme directory', async () => {
+      vi.mocked(hasShopifyThemeLoaded).mockResolvedValue(false);
+
+      await showShopifyMagicButton();
+
+      expect(commands.executeCommand).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('showShopifyMagicLoadingButton', async () => {
+    it('should show the loading component and hide the magic button when in a theme directory', async () => {
+      vi.mocked(hasShopifyThemeLoaded).mockResolvedValue(true);
+
+      await showShopifyMagicLoadingButton();
+
+      const shopifyMagicButton = 'shopifyLiquid.shopifyMagic.visible';
+      const shopifyMagicLoading = 'shopifyLiquid.shopifyMagicLoading.visible';
+
+      expect(commands.executeCommand).toBeCalledWith('setContext', shopifyMagicButton, false);
+      expect(commands.executeCommand).toBeCalledWith('setContext', shopifyMagicLoading, true);
+    });
+
+    it('should not update button visibility when not in a theme directory', async () => {
+      vi.mocked(hasShopifyThemeLoaded).mockResolvedValue(false);
+
+      await showShopifyMagicLoadingButton();
+
+      expect(commands.executeCommand).not.toHaveBeenCalled();
+    });
+  });
+});
