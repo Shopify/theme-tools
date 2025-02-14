8c7171bc33e54a302c73e5b92f62e364dd327ef2
Six Ash <six.ash@shopify.com>
2025-02-13 21:56:49 -0500

    adjust rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,18 @@ const config = [
   ...shopifyEslintPlugin.configs.typescript,
   ...shopifyEslintPlugin.configs['typescript-type-checking'],
   ...shopifyEslintPlugin.configs.node,
+
+  // @shopify/eslint-plugin requires prettier 3+!
+  // we can manually disable problematic rules instead.
+  {
+    rules: {
+      '@shopify/class-property-semi': 'off',
+      '@shopify/binary-assignment-parens': 'off',
+      'prefer-arrow-callback': 'off',
+      'arrow-body-style': 'off',
+    },
+  },
+
   includeIgnoreFile(gitignorePath),
   {
     languageOptions: {
@@ -36,6 +48,29 @@ const config = [
       'import/no-extraneous-dependencies': 'off',
       'id-length': 'off',
       curly: ['error', 'multi-line', 'consistent'],
+    },
+  },
+  {
+    // if we want to *enforce* rather than relax any ts-eslint rules, the config
+    // block's `files` needs to be a subset of the upstream `files` scope for
+    // which the ts-eslint plugin is defined. otherwise eslint will crash :(
+    // https://github.com/Shopify/web-configs/blob/main/packages/eslint-plugin/lib/config/typescript.js
+    // one of several spooky-action-at-a-distance footguns of the new flat config!
+    files: ['**/*.ts', '**/*.tsx'],
+    rules: {
+      '@typescript-eslint/no-shadow': 'warn',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
     },
   },
   {
