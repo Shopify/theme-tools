bd0ac6f74e4255f372ecb95e3e1b8890bccbf7ed
James Meng <james.meng@shopify.com>
2025-02-15 13:35:04 -0800

    [LiquidDoc] Add support for implicit description content

--- packages/liquid-html-parser/src/stage-1-cst.ts
+++ packages/liquid-html-parser/src/stage-1-cst.ts
@@ -1380,7 +1380,22 @@ function toLiquidDocAST(source: string, matchingSource: string, offset: number) 
   };
 
   const LiquidDocMappings: Mapping = {
-    Node: 0,
+    Node(implicitDescription: Node, body: Node) {
+      const self = this as any;
+      const implicitDescriptionNode =
+        implicitDescription.sourceString.length === 0
+          ? []
+          : [implicitDescription.toAST(self.args.mapping)];
+      return implicitDescriptionNode.concat(body.toAST(self.args.mapping));
+    },
+    ImplicitDescription: {
+      type: ConcreteNodeTypes.LiquidDocDescriptionNode,
+      name: 'description',
+      locStart,
+      locEnd,
+      source,
+      content: 1,
+    },
     TextNode: textNode,
     paramNode: {
       type: ConcreteNodeTypes.LiquidDocParamNode,
