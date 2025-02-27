7864d0a31c617e9b1acfa78864d852c44ef7774e
James Meng <james.meng@shopify.com>
2025-02-27 08:35:58 -0800

    --wip-- [skip ci]

---
+++ packages/theme-check-common/src/checks/missing-render-snippet-params/index.ts
@@ -0,0 +1,97 @@
+import { LiquidCheckDefinition, Severity, SourceCodeType } from '../../types';
+import { LiquidNamedArgument, RenderMarkup } from '@shopify/liquid-html-parser';
+import { toLiquidHtmlAST } from '@shopify/liquid-html-parser';
+import { getSnippetDefinition, LiquidDocParameter } from '../../liquid-doc/liquidDoc';
+import { isLiquidString } from '../utils';
+import { getDefaultValueForType } from '../../liquid-doc/utils';
+
+export const MissingRenderSnippetParams: LiquidCheckDefinition = {
+  meta: {
+    code: 'MissingRenderSnippetParams',
+    name: 'Missing Render Snippet Parameters',
+
+    docs: {
+      description:
+        'This check ensures that all required parameters are provided when rendering a snippet.',
+      recommended: true,
+      url: 'https://shopify.dev/docs/storefronts/themes/tools/theme-check/checks/missing-render-snippet-params',
+    },
+    type: SourceCodeType.LiquidHtml,
+    severity: Severity.WARNING,
+    schema: {},
+    targets: [],
+  },
+
+  create(context) {
+    function reportMissingParams(
+      missingRequiredParams: LiquidDocParameter[],
+      node: RenderMarkup,
+      snippetName: string,
+    ) {
+      for (const param of missingRequiredParams) {
+        context.report({
+          message: `Missing required parameter '${param.name}' in render tag for snippet '${snippetName}'`,
+          startIndex: node.position.start,
+          endIndex: node.position.end,
+          suggest: [
+            {
+              message: `Add required parameter '${param.name}'`,
+              fix: (fixer) => {
+                const paramToAdd = `, ${param.name}: ${getDefaultValueForType(param.type)}`;
+
+                if (node.args.length == 0) {
+                  return fixer.insert(node.snippet.position.end, paramToAdd);
+                }
+
+                const lastArg = node.args[node.args.length - 1];
+                const sourceAfterLastArg = node.source.substring(
+                  lastArg.position.end,
+                  node.position.end,
+                );
+
+                const trailingCommaAndWhitespaceMatch = sourceAfterLastArg.match(/\s*,\s*/);
+                if (trailingCommaAndWhitespaceMatch) {
+                  // IF there is already a trailing comma after the last arg, we want to find it and replace it with our own while stripping whitespace
+                  return fixer.replace(
+                    lastArg.position.end,
+                    lastArg.position.end + trailingCommaAndWhitespaceMatch[0].length,
+                    `${paramToAdd} `,
+                  );
+                }
+
+                return fixer.insert(lastArg.position.end, paramToAdd);
+              },
+            },
+          ],
+        });
+      }
+    }
+
+    return {
+      async RenderMarkup(node: RenderMarkup) {
+        if (!isLiquidString(node.snippet) || node.variable) {
+          return;
+        }
+
+        const snippetName = node.snippet.value;
+        const snippetPath = `snippets/${snippetName}.liquid`;
+        const snippetUri = context.toUri(snippetPath);
+
+        const snippetContent = await context.fs.readFile(snippetUri);
+        const snippetAst = toLiquidHtmlAST(snippetContent);
+        const snippetDef = getSnippetDefinition(snippetAst, snippetName);
+
+        if (!snippetDef.liquidDoc?.parameters) {
+          return;
+        }
+
+        const providedParams = new Map(node.args.map((arg) => [arg.name, arg]));
+        const missingRequiredParams = snippetDef.liquidDoc.parameters.filter(
+          (p) => p.required && !providedParams.has(p.name),
+        );
+
+        reportMissingParams(missingRequiredParams, node, snippetName);
+      },
+    };
+  },
+};
