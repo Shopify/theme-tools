374ff3ded1a6b0908640c477741d0874f4491ae4
Alok Swamy <alok.swamy@shopify.com>
2025-08-26 14:18:23 -0400

    Allow boolean expressions in schema tag

---
+++ .changeset/seven-suits-tan.md
@@ -0,0 +1,5 @@
+---
+'@shopify/theme-check-common': patch
+---
+
+Allow boolean expressions in schema tag
--- packages/theme-check-common/src/checks/liquid-html-syntax-error/checks/InvalidBooleanExpressions.ts
+++ packages/theme-check-common/src/checks/liquid-html-syntax-error/checks/InvalidBooleanExpressions.ts
@@ -1,4 +1,4 @@
-import { NodeTypes } from '@shopify/liquid-html-parser';
+import { NamedTags, NodeTypes } from '@shopify/liquid-html-parser';
 import { Check, Context, SourceCodeType } from '../../..';
 import { INVALID_SYNTAX_MESSAGE } from './utils';
 
@@ -6,8 +6,17 @@ export function detectInvalidBooleanExpressions(
   context: Context<SourceCodeType.LiquidHtml>,
 ): Check<SourceCodeType.LiquidHtml> {
   return {
-    async BooleanExpression(node) {
+    async BooleanExpression(node, ancestors) {
       const condition = node.condition;
+
+      // We are okay with boolean expressions in schema tags (specifically for visible_if)
+      if (
+        ancestors.some(
+          (ancestor) => ancestor.type === NodeTypes.LiquidRawTag && ancestor.name === 'schema',
+        )
+      ) {
+        return;
+      }
 
       if (
         condition.type !== NodeTypes.Comparison &&
