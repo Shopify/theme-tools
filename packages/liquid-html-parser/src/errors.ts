eeefc1c83037fdd99e57ff57bc193c60e63e07a7
SAY-5 <SAY-5@users.noreply.github.com>
2026-04-15 18:00:29 -0700

    liquid-html-parser: fix 'occured' -> 'occurred' comment typos in errors.ts
    Two inline comments in packages/liquid-html-parser/src/errors.ts explaining where the babel parser prints the error read 'the parsing error occured'. Doc-only TS change.
    
    Signed-off-by: SAY-5 <SAY-5@users.noreply.github.com>

--- packages/liquid-html-parser/src/errors.ts
+++ packages/liquid-html-parser/src/errors.ts
@@ -19,7 +19,7 @@ export class LiquidHTMLCSTParsingError extends SyntaxError {
     const lineCol = lineColumn(input).fromIndex(Math.min(errorPos, input.length - 1));
 
     // Plugging ourselves into @babel/code-frame since this is how
-    // the babel parser can print where the parsing error occured.
+    // the babel parser can print where the parsing error occurred.
     // https://github.com/prettier/prettier/blob/cd4a57b113177c105a7ceb94e71f3a5a53535b81/src/main/parser.js
     if (lineCol) {
       this.loc = {
@@ -58,7 +58,7 @@ export class LiquidHTMLASTParsingError extends SyntaxError {
     const end = lc.fromIndex(Math.min(endIndex, source.length - 1));
 
     // Plugging ourselves into @babel/code-frame since this is how
-    // the babel parser can print where the parsing error occured.
+    // the babel parser can print where the parsing error occurred.
     // https://github.com/prettier/prettier/blob/cd4a57b113177c105a7ceb94e71f3a5a53535b81/src/main/parser.js
     this.loc = {
       start: {
